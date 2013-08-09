c473e206c9d9a6c64ef8d6e0583c6431972d0c49
greenteamer <greenteamer@bk.ru>
2013-08-10 05:33:36 +0700

    Таблица материалов - начало

--- Calculator/Calculator/TestTableViewController.h
+++ Calculator/Calculator/TestTableViewController.h
@@ -2,12 +2,12 @@
 //  TestTableViewController.h
 //  Calculator
 //
-//  Created by Александр Коровкин on 09.08.13.
+//  Created by Александр Коровкин on 10.08.13.
 //  Copyright (c) 2013 Александр Коровкин. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface TestTableViewController : UITableViewCell
+@interface TestTableViewController : UITableViewController
 
 @end

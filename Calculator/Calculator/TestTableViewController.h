853e0f2d083424ddb6c2c3f180680ff119bb9cfc
greenteamer <greenteamer@bk.ru>
2013-08-09 23:16:20 +0700

    начало тестовой таблицы (коммит перед пулом)

---
+++ Calculator/Calculator/TestTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  TestTableViewController.h
+//  Calculator
+//
+//  Created by Александр Коровкин on 09.08.13.
+//  Copyright (c) 2013 Александр Коровкин. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TestTableViewController : UITableViewCell
+
+@end
---
+++ Calculator/Calculator/TestTableViewController.m
@@ -0,0 +1,29 @@
+//
+//  TestTableViewController.m
+//  Calculator
+//
+//  Created by Александр Коровкин on 09.08.13.
+//  Copyright (c) 2013 Александр Коровкин. All rights reserved.
+//
+
+#import "TestTableViewController.h"
+
+@implementation TestTableViewController
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end

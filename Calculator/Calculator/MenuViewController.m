7b82b1d94c93a69d0ab171265abbcb08aa2fb889
greenteamer <greenteamer@bk.ru>
2013-08-23 05:43:08 +0700

    начало проектов

--- Calculator/Calculator/MenuViewController.m
+++ Calculator/Calculator/MenuViewController.m
@@ -38,7 +38,7 @@
     // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
     
-    self.menu = [NSArray arrayWithObjects:@"Главная", @"Настройки", @"Чертеж", @"Калькулятор", @"E-Mail", nil];
+    self.menu = [NSArray arrayWithObjects:@"Главная", @"Настройки", @"Чертеж", @"Калькулятор", @"E-Mail", @"Проект", nil];
     
     
     [self.slidingViewController setAnchorRightRevealAmount:200.0f];

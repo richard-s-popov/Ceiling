fa9d2820fe77b29b8aea7c9928cd3dd1baf97def
Aleksandr <greenteamer@bk.ru>
2013-11-10 00:55:38 +0700

    добавлен подсчет стоимости проекта

--- Calculator/Projects.m
+++ Calculator/Projects.m
@@ -2,7 +2,7 @@
 //  Projects.m
 //  Calculator
 //
-//  Created by Александр Коровкин on 09.11.13.
+//  Created by Александр Коровкин on 10.11.13.
 //  Copyright (c) 2013 Александр Коровкин. All rights reserved.
 //
 
@@ -16,6 +16,7 @@
 @dynamic projectAdress;
 @dynamic projectName;
 @dynamic projectPhone;
+@dynamic projectPrice;
 @dynamic projectPlot;
 
 @end

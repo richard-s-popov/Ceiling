b7a74834dcfa802b1a04a03a66d19acd28c89699
greenteamer <greenteamer@bk.ru>
2015-08-08 15:08:33 +0600

    start work with materials

--- Calculator/Calculator/PlotDiagonalViewController.m
+++ Calculator/Calculator/PlotDiagonalViewController.m
@@ -78,7 +78,8 @@
     [diagonalConteinerView addSubview:viewPlotButton];
     
     //кнопка для пометки шва
-    UIImage *shovBackground = [[UIImage imageNamed:@"greenBg.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
+//    UIImage *shovBackground = [[UIImage imageNamed:@"greenBg.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
+    UIImage *shovBackground = [[UIImage imageNamed:@"checkbox.png"] resizableImageWithCapInsets:UIEdgeInsetsMake(0, 0, 0, 0)];
     UIButton *shovButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     shovButton.frame = CGRectMake(0, 50, 320, 50);
     [shovButton setTitle:[NSString stringWithFormat:@"Установить сторону %@%@ как начало шва", side.angleFirst, side.angleSecond] forState:UIControlStateNormal];
@@ -87,6 +88,7 @@
 //    [shovButton setBackgroundColor:[UIColor greenColor]];
     [shovButton addTarget:self action:@selector(addShov) forControlEvents:UIControlEventTouchUpInside];
     [diagonalConteinerView addSubview:shovButton];
+    
     
     //создание надписи start
     startLable = [[UILabel alloc] initWithFrame:CGRectMake(30, 104, 320, 30)];
